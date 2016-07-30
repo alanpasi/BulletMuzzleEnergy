2c9f10d3c10b84022224c2a9c33c03f371874338
alanpasi <alanpasi@gmail.com>
2016-07-30 15:22:54 -0300

    Functional for US Unit System

--- app/src/main/java/com/alanpasi/bulletmuzzleenergy/MainActivity.java
+++ app/src/main/java/com/alanpasi/bulletmuzzleenergy/MainActivity.java
@@ -1,13 +1,52 @@
 package com.alanpasi.bulletmuzzleenergy;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etMass;
+    EditText etVelocity;
+    TextView tvEnergyResult;
+
+    final double ACCELERATION_DUE_TO_GRAVITY = 7000d;
+    final double FOOT_POUND_FORCE = (1d / (ACCELERATION_DUE_TO_GRAVITY * 32.13d));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etMass = (EditText) findViewById(R.id.etMass);
+        etVelocity = (EditText) findViewById(R.id.etVelocity);
+        tvEnergyResult = (TextView) findViewById(R.id.tvEnergyResult);
+
+        tvEnergyResult.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        double mass;
+        double velocity;
+        double result;
+
+        mass = Double.parseDouble(etMass.getText().toString());
+        velocity = Double.parseDouble(etVelocity.getText().toString());
+
+        result = ((1d/2d) * (mass * (velocity * velocity))) * FOOT_POUND_FORCE;
+
+//        tvEnergyResult.setText(etMass.getText().toString());
+
+        DecimalFormat decimalFormat = new DecimalFormat("#,###.##");
+        tvEnergyResult.setText(decimalFormat.format(result));
+
     }
 }
